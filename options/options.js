2129e4daa1db9e30d455451799a287a38eb070f9
Crowdbotics <team@crowdbotics.com>
2024-03-11 15:44:17 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "app-for-event-manag-46927",
+  url: "https://app-for-event-manag-46927.botics.co",
+  api: "https://app-for-event-manag-46927.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
