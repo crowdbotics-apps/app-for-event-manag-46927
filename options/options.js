37b079d0dc016bed1285a0596ccb5bafecbfe7be
Liz Pham <liz.pham@avanade.com>
2024-03-11 11:18:14 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "app-for-event-manag-46927",
-  url: "https://app-for-event-manag-46927.botics.co",
-  api: "https://app-for-event-manag-46927.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "app-for-event-manag-46927",
+  "url": "https://app-for-event-manag-46927.botics.co",
+  "api": "https://app-for-event-manag-46927.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
